06cc53f61ba06e4e43c5602741aae6c174069bf1
Mr-Xcoder <vdxcoder123@gmail.com>
2017-07-16 22:33:15 +0300

    Update Cthulhu.py

--- Cthulhu.py
+++ Cthulhu.py
@@ -5,75 +5,48 @@ class properties(dict):
 
 functions = {
 
-    # Operator Functions
-
+    # OPERATOR FUNCTIONS
     # Operators with a precendece of 1 (Irrelevant in Cthulhu, but a nice sorting criterion)
 
     '+' : properties(
-
         # Addition
-
         arity = 2,
         function = lambda a,b: a + b
     ),
-
     '-' : properties(
-
         # Subtraction
-
         arity = 2,
         function = lambda a,b: a - b
     ),
-
     '#' : properties(
-
         # Absolute Difference
-
         arity = 2,
         function = lambda a,b: abs(a - b)
-
     ),
-
     '±' : properties(
-
         # Solve both Adittion and Sutraction, returns a tuple with the first value being the sum and the other one the difference
-
         arity = 2,
         function = lambda a,b: (a + b, a - b)
-
     ),
 
     # Operators with Precedence of 2 (Irrelevant in Cthulhu, but a nice sorting criterion)
-
     '*' : properties(
-
         # Multiplication
-
         arity = 2,
         function = lambda a,b: a * b
-
     ),
-
     '/' : properties(
-
         # Division
-
         arity = 2,
         function = lambda a,b: a / b
-
     ),
-
     '÷' : properties(
-
         # Integer division
-
         arity = 2,
         function = lambda a,b: a // b
-
     ),
 
     # Operators with Precedence of 3 (Irrelevant in Cthulhu, but a nice sorting criterion)
-
     '^' : properties(
 
         # Raise the first value to the power of the second value
@@ -84,63 +57,38 @@ functions = {
     ),
 
     # Comparison Operators
-
     '=' : properties(
-
         # Check if the values provided are equal
-
         arity = 2,
         function = lambda a,b: a == b
-
     ),
-
     '≠' : properties(
-
         # Check if the values provided are not equal
-
         arity = 2,
         function = lambda a,b: a != b
-
     ),
-
     '<' : properties(
-
         # Check if the first value is smaller than the second value
-
         arity = 2,
         function = lambda a,b: a < b
-
     ),
-
     '>': properties(
-
         # Check if the first value is higher than the second value
-
         arity = 2,
         function = lambda a,b: a > b
-
     ),
-
     '≤': properties(
-
         # Check if the first value is smaller than or equal to the second value
-
         arity = 2,
         function = lambda a,b: a <= b
-
     ),
-
     '≥': properties(
-
         # Check if the first value is greater than or equal to the second value
-
         arity = 2,
         function = lambda a,b: a >= b
-
     ),
 
     # Standard functions
-
     'ª' : properties(
         arity = 2,
         function = list.append
